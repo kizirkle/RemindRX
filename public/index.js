0dab1ea6bebfded3aff143ec5eb72d93a6eefb4a
zoehopebaker <zoehopebaker@gmail.com>
2026-03-23 20:57:49 -0400

    Front end for password checking and test file
    Password requirement make items red if they are not satisfied. Added a file and some Jest code to start test cases (not working completely right at the moment)

--- public/index.js
+++ public/index.js
@@ -8,13 +8,23 @@ var loginForm = document.getElementById('login-form')
 var radioPatientCreate = document.getElementById('radio-patient')
 var radioProviderCreate = document.getElementById('radio-provider')
 
-//Infomration from form needed to create a new patient or provider and log them in 
+//Information from form needed to create a new patient or provider and log them in 
 var firstName = document.getElementById('first-name')
 var lastName = document.getElementById('last-name')
 var phoneNumber = document.getElementById('phone-number')
 var email = document.getElementById('email')
 var newPassword = document.getElementById('new-password')
 var confirmedPassword = document.getElementById('confirmed-password')
+
+//List of password requirements 
+var length = document.getElementById('length')
+var number = document.getElementById('number')
+var lower = document.getElementById('lower')
+var upper = document.getElementById('upper')
+var special = document.getElementById('special')
+
+//Password requirement list
+var passwordList = document.getElementById('password-requirements-list')
 
 //Hidden provider ID field that appears when provider radio button is chosen 
 var providerIdField = document.getElementById("provider-id-field")
@@ -55,7 +65,7 @@ if(createAccountForm){
 
             //Gets the response from the API call which will either contain an error message or URL to personal portal
             var data = await response.json()
-            //console.log(data.passwordProblems)
+            console.log(data.passwordProblems)
 
             //Calls function to either return an error or sends the patient or provider to their personal portal
             systemResponse(data, type)
@@ -100,10 +110,23 @@ if(loginForm) {
 
 //Returns an error or sends the patient or provider to their personal portal after attempting to log in or sign up 
 function systemResponse(data, type) {
+    //Changes the color of the passwords to red 
+
     if(!data.passed) {
         //If there was an error, report the given error and reload the current page
         //Stores the error message in session storge to be displayed when the page is reloaded
         sessionStorage.setItem('errorMessage', data.message)
+        console.log("here")
+
+        if(data.message === "Invalid password") {
+            passwordAlert(data)
+        } else {
+            sessionStorage.setItem("short", "false")
+            sessionStorage.setItem("noNumber", "false")
+            sessionStorage.setItem("noLower", "false")
+            sessionStorage.setItem("noUpper", "false")
+            sessionStorage.setItem("noSpecial", "false")
+        }
         //Reloads the page
         location.reload()
     } else {
@@ -116,6 +139,37 @@ function systemResponse(data, type) {
     }
 }
 
+//Checks which password requirements were not satisfied 
+function passwordAlert(data) {
+    var passwordProblems = data.passwordProblems
+    if (passwordProblems.includes("short")) {
+        sessionStorage.setItem("short", "true")
+    } else {
+        sessionStorage.setItem("short", "true")
+    }
+    if (passwordProblems.includes("no number")) {
+        sessionStorage.setItem("noNumber", "true")
+    } else{
+        sessionStorage.setItem("noNumber", "false")
+    }
+    if (passwordProblems.includes("no lowercase")) {
+        sessionStorage.setItem("noLower", "true")
+    } else {
+        sessionStorage.setItem("noLower", "false")
+    }
+    if (passwordProblems.includes("no uppercase")) {
+        sessionStorage.setItem("noUpper", "true")
+    } else {
+        sessionStorage.setItem("noUpper", "false")
+    }
+    if (passwordProblems.includes("no special character")) {
+        console.log("no special")
+        sessionStorage.setItem("noSpecial", "true")
+    } else {
+        sessionStorage.setItem("noSpecial", "false")
+    }
+}
+
 //Each time the page is reloaded, report the error from the previous log in or sign up attempt if one exists
 window.onload = function () {
     //Attempt to get the error message from session storage
@@ -125,8 +179,48 @@ window.onload = function () {
         message.textContent = errorMessage
         message.style.display = "block"
     }
+
+    //Determines which password requirements were satisfied or not
+    var short = sessionStorage.getItem('short')
+    var noNumber = sessionStorage.getItem('noNumber')
+    var noLower = sessionStorage.getItem('noLower')
+    var noUpper = sessionStorage.getItem('noUpper')
+    var noSpecial = sessionStorage.getItem('noSpecial')
+
+    //Changes color of list of password requirement to red if not satisfied
+    if (short === "true") {
+        length.style.color = "red"
+    } else {
+        length.style.color = "black"
+    }
+    if (noNumber === "true") {
+        number.style.color = "red"
+    } else {
+        number.style.color = "black"
+    }
+    if (noLower === "true") {
+        lower.style.color = "red"
+    } else {
+        lower.style.color = "black"
+    } 
+    if (noUpper === "true") {
+        upper.style.color = "red"
+    } else {
+        upper.style.color = "black"
+    }
+    if (noSpecial === "true") {
+        special.style.color = "red"
+    } else {
+        special.style.color = "black"
+    }
+
     //Rermoves the error message from session storage after it has been reported 
     sessionStorage.removeItem('errorMessage');
+    sessionStorage.removeItem('short');
+    sessionStorage.removeItem('noNumber');
+    sessionStorage.removeItem('noLower');
+    sessionStorage.removeItem('noUpper');
+    sessionStorage.removeItem('noSpecial');
 }
 
 //If the patient radio button is clicked when creating an account, add the proivder ID field
